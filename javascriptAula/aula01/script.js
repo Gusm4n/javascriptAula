559c5fb2faf61e40cb0b43d6cecc044505662e9e
Felipe <felipe.unip.adm2015@gmail.com>
2021-02-15 02:04:46 -0300

    Primeiras aulas

--- javascriptAula/aula01/script.js
+++ javascriptAula/aula01/script.js
@@ -1,1 +1,24 @@
-console.log()
+// Declarar uma variável com o seu nome
+
+const nome = 'Felipe';
+
+// Declarar uma variável com a sua idade
+
+let idade = 24;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+let comidaFavorita; 
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'Pizza';
+console.log(comidaFavorita)
+// Declarar 5 variáveis diferentes sem valores
+
+var cidadeNatal,
+    carroFavorito,
+    bairroFavorito,
+    numeroSapato,
+    marcaRelogio;
---
+++ javascriptAula/aula02/script.js
@@ -0,0 +1,31 @@
+// Declare uma variável contendo uma string
+
+var minhaRua = 'Ananias Batista de Menezes';
+
+// Declare uma variável contendo um número dentro de uma string
+
+var numeroCasa = 394;
+
+// Declare uma variável com a sua idade
+
+var idade = 24;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var nome = 'Felipe';
+var sobrenome = 'Gusmão';
+
+var nomeCompleto = `${nome}  ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = `It's time`;
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+var verifica = typeof(nome);
+console.log(verifica);
