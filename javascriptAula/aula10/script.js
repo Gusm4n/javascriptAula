30ab2b0e2716f163bb97b9c1d12bee50c7a5286a
Felipe <felipe.unip.adm2015@gmail.com>
2021-02-16 17:00:50 -0300

    DOM basico

---
+++ javascriptAula/aula10/script.js
@@ -0,0 +1,19 @@
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(window.document.URL);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+var ativo = document.querySelector('.ativo');
+console.log(ativo)
+
+
+// Retorne a linguagem do navegador
+
+const linguagem = navigator.language;
+console.log(linguagem)
+
+// Retorne a largura da janela 
+
+console.log(window.innerWidth)
