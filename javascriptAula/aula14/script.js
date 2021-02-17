aba0d85d6f00a5399578428a6bb8390a8f949a53
Felipe <felipe.unip.adm2015@gmail.com>
2021-02-17 20:56:08 -0300

    Dimensões

---
+++ javascriptAula/aula14/script.js
@@ -0,0 +1,54 @@
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+
+const imagem = document.querySelector('img');
+
+console.log(imagem.offsetTop)
+
+// Retorne a soma da largura de todas as imagens
+
+function somaImagens() {
+
+    const todasImagens = document.querySelectorAll('img');
+
+    let soma = 0;
+
+    todasImagens.forEach(function (item) {
+        soma = soma + item.offsetWidth;
+        console.log(item.offsetWidth)
+        
+    });
+    console.log(soma);
+}
+window.onload = function () {
+    somaImagens();
+}
+
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('a');
+
+links.forEach((link) => {
+    const linkWidth = link.offsetWidth;
+    const linkHeight = link.offsetHeight;
+    if(linkWidth >= 48 && linkHeight >= 48){
+        console.log(link, 'Possui acessibilidade.')
+    }else{
+        console.log(link, 'Não possui acessibilidade.')
+    }
+})
+
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const browserMobile = window.matchMedia('(max-width: 720px)').matches;
+
+if(browserMobile){
+    const menu = document.querySelector('.menu');
+    menu.classList.add('menuMobile');
+}
+
